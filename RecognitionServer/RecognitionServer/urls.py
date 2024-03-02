034edb4f3fa5ea3b2e780b301bd22600352066af
Soonnick0123 <soonnick02@gmail.com>
2024-03-02 16:20:45 +0800

    update

--- RecognitionServer/RecognitionServer/urls.py
+++ RecognitionServer/RecognitionServer/urls.py
@@ -4,6 +4,6 @@ from RecognitionServer import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello_world', views.hello_world),
-    path('second_message', views.second_function),
+    path("helloWorld", views.helloWorld),
+    path("secondWessage", views.secondFunction),
 ]
--- RecognitionServer/RecognitionServer/views.py
+++ RecognitionServer/RecognitionServer/views.py
@@ -3,9 +3,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse,JsonResponse
 
 @csrf_exempt
-def hello_world ( request ):
+def helloWorld ( request ):
     return JsonResponse({ 'message': 'Hello, world!' })
 
 @csrf_exempt
-def second_function(request):
+def secondFunction(request):
     return JsonResponse({'message': "Second Message!"})
