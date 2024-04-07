03c950d5c7e64a35f1791dfdaf47cbd9927c9370
Soonnick0123 <soonnick02@gmail.com>
2024-04-08 00:53:26 +0800

    update

--- RecognitionServer/RecognitionApp/admin.py
+++ RecognitionServer/RecognitionApp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'gender')
+    list_display = ("id",'name', 'email', 'phone', 'gender')
     search_fields = ('name', 'email')
 
 admin.site.register(Customer, CustomerAdmin)
